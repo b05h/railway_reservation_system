5e94dd04db1ec5b7267a946215b77a20377b24b5
Athul Anoop <athulanoop2020@gmail.com>
2025-08-29 08:25:24 +0530

    feat: input validation

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,33 +1,52 @@
 import bcrypt from "bcrypt";
 import { asyncErrorHandler } from "../utils/errors.js";
 import User from "../models/userModel.js";
+import { AppError } from "../utils/errors.js";
 import { getToken } from "../utils/jwt.js";
+import * as z from "zod";
 
 const signUp = asyncErrorHandler(async (req, res, next) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "Missing required fields" });
-  }
+  0;
+  // validate request body
+  const schema = z.object({
+    name: z.string().min(3).max(50),
+    email: z.email(),
+    password: z.string().min(8),
+  });
+  const { name, email, password } = await schema.parseAsync(req.body);
+
+  // hash password
   const hashedPassword = await bcrypt.hash(password, 10);
+
+  // create user
   const user = await User.create(name, email, hashedPassword);
-  res.status(201).json(user);
+
+  res.success({ user }, { status: 201 });
 });
 
 const signIn = asyncErrorHandler(async (req, res, next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ message: "Missing required fields" });
-  }
+  const schema = z.object({
+    email: z.email(),
+    password: z.string(),
+  });
+
+  const { email, password } = await schema.parseAsync(req.body);
+
+  // find user by email and throw error if not found
   const user = await User.findByEmail(email);
   if (!user) {
-    return res.status(401).json({ message: "Invalid email or password" });
+    throw new AppError(404, "Invalid email or password");
   }
+
+  // check password and throw error if invalid
   const validPassword = await bcrypt.compare(password, user.password);
   if (!validPassword) {
-    return res.status(401).json({ message: "Invalid email or password" });
+    throw new AppError(401, "Invalid email or password");
   }
+
+  // generate token
   const token = getToken(user);
-  res.status(200).json({ token });
+  res.success({ token });
 });
 
 export default {
