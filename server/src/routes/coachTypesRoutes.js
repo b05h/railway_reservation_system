d542a6543b9a46f36fde16559f58a32211d41676
Athul Anoop <athulanoop2020@gmail.com>
2025-09-12 22:58:18 +0530

    feat: coach type module

---
+++ server/src/routes/coachTypesRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { coachTypeController } from "../controllers/index.js";
+
+const coachTypeRouter = Router();
+
+coachTypeRouter.get("/", coachTypeController.get);
+coachTypeRouter.get("/id", coachTypeController.getById);
+coachTypeRouter.post("/", coachTypeController.create);
+coachTypeRouter.patch("/:id", coachTypeController.update);
+coachTypeRouter.delete("/:id", coachTypeController.remove);
+
+export default coachTypeRouter;
