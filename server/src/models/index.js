44aec07d057e3b6f710987292da2120b057f6109
Athul Anoop <athulanoop2020@gmail.com>
2025-09-12 22:59:03 +0530

    chore(exports): update re-exports from models, controllers and routes

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,3 +3,6 @@ export { default as Audit } from "./auditLogModel.js";
 export { default as Train } from "./trainModel.js";
 export { default as Station } from "./stationModel.js";
 export { default as StationDistance } from "./stationDistanceModel.js";
+export { default as Schedule } from "./scheduleModel.js";
+export { default as ScheduleStops } from "./scheduleStopsModel.js";
+export { default as CoachType } from "./coachTypesModel.js";
