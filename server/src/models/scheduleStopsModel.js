a22341522d1417ddb46f7bd4df8a6cdfc9783b4a
Athul Anoop <athulanoop2020@gmail.com>
2025-09-12 22:58:40 +0530

    feat: schedule module with schedule stops definition

---
+++ server/src/models/scheduleStopsModel.js
@@ -0,0 +1,93 @@
+import { queryDB } from "../utils/db.js";
+
+class ScheduleStopsModel {
+  static async find(filter, sort) {
+    const query = `SELECT * FROM schedule_stops`;
+    const values = [];
+    const conditions = [];
+
+    if (filter.id) {
+      conditions.push(`id = $${values.length + 1}`);
+      values.push(filter.id);
+    }
+    if (filter.schedule_id) {
+      conditions.push(`schedule_id = $${values.length + 1}`);
+      values.push(filter.scheduleId);
+    }
+    if (filter.station_id) {
+      conditions.push(`station_id = $${values.length + 1}`);
+      values.push(filter.stationId);
+    }
+    if (filter.stop_number) {
+      conditions.push(`stop_number = $${values.length + 1}`);
+      values.push(filter.stopNumber);
+    }
+    if (filter.arrival_time) {
+      conditions.push(`arrival_time = $${values.length + 1}`);
+      values.push(filter.arrivalTime);
+    }
+    if (filter.departure_time) {
+      conditions.push(`departure_time = $${values.length + 1}`);
+      values.push(filter.departureTime);
+    }
+
+    if (conditions.length > 0) {
+      query += ` WHERE ${conditions.join(" AND ")}`;
+    }
+
+    const sortableFields = ["stop_number", "arrival_time", "departure_time"];
+
+    if (sort.sortBy && sortableFields.includes(sort.sortBy)) {
+      const sortOrder = sort.sortOrder || "ASC";
+      query += ` ORDER BY ${sort.sortBy} ${sortOrder}`;
+    }
+
+    if (filter.limit) {
+      query += ` LIMIT $${values.length + 1} `;
+      values.push(filter.limit);
+    }
+    if (filter.page) {
+      query += ` OFFSET $${values.length + 1} `;
+      values.push((filter.page - 1) * filter.limit);
+    }
+
+    const result = await queryDB(query, values);
+    return result.rows;
+  }
+
+  static async create(scheduleStop) {
+    const query = `INSERT INTO schedule_stops (schedule_id, station_id, stop_number, arrival_time, departure_time) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+    const values = [
+      scheduleStop.scheduleId,
+      scheduleStop.stationId,
+      scheduleStop.stopNumber,
+      scheduleStop.arrivalTime,
+      scheduleStop.departureTime,
+    ];
+    const result = await queryDB(query, values);
+    return result.rows[0];
+  }
+
+  static async update(scheduleStop) {
+    const query = `UPDATE schedule_stops SET schedule_id = $1, station_id = $2, stop_number = $3, arrival_time = $4, departure_time = $5 WHERE id = $6 RETURNING *`;
+    const values = [
+      scheduleStop.scheduleId,
+      scheduleStop.stationId,
+      scheduleStop.stopNumber,
+      scheduleStop.arrivalTime,
+      scheduleStop.departureTime,
+      scheduleStop.id,
+    ];
+    const result = await queryDB(query, values);
+    return result.rows[0];
+  }
+
+  static async delete(id) {
+    const query = `DELETE FROM schedule_stops WHERE id = $1 RETURNING *`;
+    const values = [id];
+    const result = await queryDB(query, values);
+    return result.rows[0];
+  }
+}
+
+export default ScheduleStopsModel;
