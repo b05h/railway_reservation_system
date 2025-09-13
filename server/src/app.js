0c7e07a7fd81021bcea52d9cc3a0ea9b7046fa2b
Athul Anoop <athulanoop2020@gmail.com>
2025-09-13 14:25:12 +0530

    fix: allowed method types for requests

--- server/src/app.js
+++ server/src/app.js
@@ -23,7 +23,7 @@ export default function createApp(config) {
     cors({
       origin: ["http://localhost:5173"],
       credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization"],
     }),
   );
