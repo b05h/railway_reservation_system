f18516c9eb1912d0ae7111dc3b339c18e5fcb8b6
b05h <bhavanahkrishnan@gmail.com>
2025-09-18 23:23:19 +0530

    consolidate imports using index.js

--- server/src/routes/trainRoutes_user.js
+++ server/src/routes/trainRoutes_user.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { trainControllerUser } from "../controllers/index.js";
 
+
 const router = express.Router();
 
 router.get("/search", trainControllerUser.searchTrains);
