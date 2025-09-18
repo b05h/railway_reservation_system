2a4f10a31dd12da2f51e48e9788f5e896d6cf453
Athul Anoop <athulanoop2020@gmail.com>
2025-09-18 12:15:23 +0530

    feat: schedule creation is accompanied by transaction based creation of schedule stops as well

--- server/src/models/scheduleModel.js
+++ server/src/models/scheduleModel.js
@@ -1,26 +1,27 @@
-import { queryDB } from "../utils/db.js";
+import { getDBClient, queryDB } from "../utils/db.js";
+import { AppError } from "../utils/errors.js";
 
 class ScheduleModel {
   static async find(filter, sort) {
-    let query = `SELECT * FROM schedules`;
+    let query = `SELECT schedules.*, array_agg(schedule_stops.id) AS schedule_stops FROM schedules JOIN schedule_stops ON schedules.id = schedule_stops.schedule_id`;
     const values = [];
     const conditions = [];
 
     if (filter) {
       if (filter.id) {
-        conditions.push(`id = $${values.length + 1}`);
+        conditions.push(`schedules.id = $${values.length + 1}`);
         values.push(filter.id);
       }
       if (filter.trainId) {
-        conditions.push(`train_id = $${values.length + 1}`);
+        conditions.push(`schedules.train_id = $${values.length + 1}`);
         values.push(filter.trainId);
       }
       if (filter.departureDate) {
-        conditions.push(`departure_date = $${values.length + 1}`);
+        conditions.push(`schedules.departure_date = $${values.length + 1}`);
         values.push(filter.departureDate);
       }
       if (filter.departureTime) {
-        conditions.push(`departure_time = $${values.length + 1}`);
+        conditions.push(`schedules.departure_time = $${values.length + 1}`);
         values.push(filter.departureTime);
       }
 
@@ -34,7 +35,7 @@ class ScheduleModel {
 
       if (sort.sortBy && sortableFields.includes(sort.sortBy)) {
         const sortOrder = sort.sortOrder || "ASC";
-        query += ` ORDER BY ${sort.sortBy} ${sortOrder}`;
+        query += ` ORDER BY schedules.${sort.sortBy} ${sortOrder}`;
       }
     }
 
@@ -49,33 +50,94 @@ class ScheduleModel {
       }
     }
 
+    query += ` GROUP BY schedules.id`;
+
     const result = await queryDB(query, values);
     return result.rows;
   }
 
   static async create(schedule) {
-    let query = `INSERT INTO schedules (train_id, departure_date, departure_time) VALUES ($1, $2, $3) RETURNING *`;
-    const values = [
-      schedule.trainId,
-      schedule.departureDate,
-      schedule.departureTime,
-    ];
-    return (await queryDB(query, values)).rows[0];
+    const client = await getDBClient();
+    try {
+      await client.query("BEGIN");
+      let query = `INSERT INTO schedules (train_id, departure_date, departure_time) VALUES ($1, $2, $3) RETURNING *`;
+      let values = [
+        schedule.trainId,
+        schedule.departureDate,
+        schedule.departureTime,
+      ];
+      let result = (await client.query(query, values)).rows[0];
+      for (const scheduleStop of schedule.scheduleStops) {
+        query = `INSERT INTO schedule_stops (schedule_id, station_id, stop_number, arrival_time, departure_time) VALUES ($1, $2, $3, $4, $5)`;
+        values = [
+          result.id,
+          scheduleStop.stationId,
+          scheduleStop.stopNumber,
+          scheduleStop.arrivalTime,
+          scheduleStop.departureTime,
+        ];
+        await client.query(query, values);
+      }
+      await client.query("COMMIT");
+      return result;
+    } catch (e) {
+      await client.query("ROLLBACK");
+      throw e;
+    } finally {
+      client.release();
+    }
   }
 
   static async update(schedule) {
-    let query = `UPDATE schedules SET train_id = $1, departure_date = $2, departure_time = $3 WHERE id = $4 RETURNING *`;
-    const values = [
-      schedule.trainId,
-      schedule.departureDate,
-      schedule.departureTime,
-      schedule.id,
-    ];
-    return (await queryDB(query, values)).rows[0];
+    const client = await getDBClient();
+    try {
+      await client.query("BEGIN");
+      let query = `UPDATE schedules SET 
+                      train_id = COALESCE($1, train_id), 
+                      departure_date = COALESCE($2, departure_date), 
+                      departure_time = COALESCE($3, departure_time), 
+                      updated_at = NOW() 
+                    WHERE id = $4 RETURNING *`;
+      let values = [
+        schedule.trainId,
+        schedule.departureDate,
+        schedule.departureTime,
+        schedule.id,
+      ];
+      let result = (await client.query(query, values)).rows[0];
+      if (!result) {
+        throw new AppError(400, "Could not update schedule");
+      }
+
+      if (schedule.scheduleStops) {
+        await client.query(
+          "DELETE FROM schedule_stops WHERE schedule_id = $1",
+          [schedule.id],
+        );
+        for (const scheduleStop of schedule.scheduleStops) {
+          query = `INSERT INTO schedule_stops (schedule_id, station_id, stop_number, arrival_time, departure_time) VALUES ($1, $2, $3, $4, $5)`;
+          values = [
+            result.id,
+            scheduleStop.stationId,
+            scheduleStop.stopNumber,
+            scheduleStop.arrivalTime,
+            scheduleStop.departureTime,
+          ];
+          await client.query(query, values);
+        }
+      }
+      await client.query("COMMIT");
+      return result;
+    } catch (e) {
+      await client.query("ROLLBACK");
+      throw e;
+    } finally {
+      client.release();
+    }
   }
 
   static async delete(id) {
-    let query = `DELETE FROM schedules WHERE id = $1`;
+    let query = `DELETE FROM schedules WHERE id = $1 RETURNING *`;
     const values = [id];
     return (await queryDB(query, values)).rows[0];
   }
